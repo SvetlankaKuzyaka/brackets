e6a749746413d81089f2bc94ddfc7b870d1ade7f
SvetlankaKuzyaka <svetlanka-kuznecov@yandex.ru>
2019-03-20 14:21:24 +0300

    add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var bufArray = [];
+  var initArray = str.split('');
+  return (initArray.every(function(item) {
+    for (var i = 0; i < bracketsConfig.length; i++) {
+      if (bracketsConfig[i].indexOf(item) == -1) continue;
+      if (bracketsConfig[i].indexOf(item) == 0 && bracketsConfig[i][0] != bracketsConfig[i][1]) {bufArray.push(i); return true;};
+      if (bracketsConfig[i].indexOf(item) == 1 && bufArray[bufArray.length-1] == i) {bufArray.pop(i); return true;};
+      if (bufArray[bufArray.length-1] == i) {bufArray.pop(i); return true;} else {bufArray.push(i); return true;};
+    }
+    return false;
+  }) && bufArray.length == 0);
 }
